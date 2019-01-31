829c9aaea075dd55d3f77fabc3e89bab77d55573
EldarTest <ziatdinov.eldar9@gmail.com>
2019-01-31 18:34:28 +0400

    extract css plugin

--- native-js/webpack.config.js
+++ native-js/webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     devtool: 'source-map',
-    entry:'./src/index.js',
+    entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'index.js',
@@ -27,8 +28,17 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'index.css',
+            allChunks: true
+        })
+    ],
 };
